5494a78547a51b939a59c28d0a4c670f3bfd046a
TorJr <joto444@icloud.com>
2024-05-12 21:39:39 -0500

    Modified by www.SourceFiles.app

---
+++ activate/activate/activate.h
@@ -0,0 +1,18 @@
+//
+//  activate.h
+//  activate
+//
+//  Created by Jose Torres on 5/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for activate.
+FOUNDATION_EXPORT double activateVersionNumber;
+
+//! Project version string for activate.
+FOUNDATION_EXPORT const unsigned char activateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <activate/PublicHeader.h>
+
+
