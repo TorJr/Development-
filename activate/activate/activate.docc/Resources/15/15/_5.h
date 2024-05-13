1db387018f5fe4f691fb4d706d01c9467bc31c76
TorJr <joto444@icloud.com>
2024-05-13 10:07:21 -0500

    Modified by www.SourceFiles.app

---
+++ activate/activate/activate.docc/Resources/15/15/_5.h
@@ -0,0 +1,18 @@
+//
+//  _5.h
+//  15
+//
+//  Created by Jose Torres on 5/13/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for _5.
+FOUNDATION_EXPORT double _5VersionNumber;
+
+//! Project version string for _5.
+FOUNDATION_EXPORT const unsigned char _5VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <_5/PublicHeader.h>
+
+
